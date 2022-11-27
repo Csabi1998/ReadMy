1fe74d0d7e7d43b440aca246429f6fcaa0366069
Czeglédi Levente <czegledi.levente@wadjet.hu>
2022-11-27 11:44:50 +0100

    add edit and participant adding to projects

--- Frontend/read-my/src/app/pages/projects/edit-project/edit-project.component.ts
+++ Frontend/read-my/src/app/pages/projects/edit-project/edit-project.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-project',
   templateUrl: './edit-project.component.html',
   styleUrls: ['./edit-project.component.css'],
 })
-export class EditProjectComponent {
+export class EditProjectComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private router: Router) {}
   isLoading = false;
+  projectId?: string;
+
+  participants = ['Participant 1', 'Participant 2'];
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.projectId = params['id'];
+    });
+  }
+
+  get isNew() {
+    return this.projectId === undefined;
+  }
 
   onSubmit(form: NgForm) {
+    console.log(form.value);
+    if (!form.valid) {
+      return;
+    }
+  }
+
+  onSubmitNewParticipant(form: NgForm) {
     console.log(form.value);
     if (!form.valid) {
       return;
