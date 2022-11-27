748b7fd2ca07159104649065cc2160daa0f44a7b
Czeglédi Levente <czegledi.levente@wadjet.hu>
2022-11-27 15:58:09 +0100

    routing finished, edit log/task component still missing

--- Frontend/read-my/src/app/pages/projects/project-card/project-card.component.ts
+++ Frontend/read-my/src/app/pages/projects/project-card/project-card.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { ProjectResponse } from './../../../api/projects/models/projectResponse';
 
@@ -7,7 +8,13 @@ import { ProjectResponse } from './../../../api/projects/models/projectResponse'
   styleUrls: ['./project-card.component.css'],
 })
 export class ProjectCardComponent {
+  constructor(private location: Location) {}
   @Input() project!: ProjectResponse;
   @Input() shouldShowDetailsButton: boolean = true;
   @Input() shouldShowEditAndDeleteButton: boolean = false;
+  @Input() shouldShowBackButton: boolean = false;
+
+  back(): void {
+    this.location.back();
+  }
 }
