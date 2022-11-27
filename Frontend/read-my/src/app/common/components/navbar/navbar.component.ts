514725e3a2bd7075236a2a8f6573773a4d0556c8
Czeglédi Levente <czegledi.levente@wadjet.hu>
2022-11-27 19:19:48 +0100

    add navbar logic and correct markup

--- Frontend/read-my/src/app/common/components/navbar/navbar.component.ts
+++ Frontend/read-my/src/app/common/components/navbar/navbar.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
+  isLoggedIn = false;
+  isAdmin = false;
 
+  private userSub!: Subscription;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe((user) => {
+      this.isLoggedIn = !!user;
+      console.log(user?.isAdmin);
+      this.isAdmin = user?.isAdmin ?? false;
+    });
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
 }
