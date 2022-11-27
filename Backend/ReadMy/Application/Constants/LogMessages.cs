3de396b2f750ee3e7d3403aeaaa3ce6b322895e0
Csabi1998 <61312438+Csabi1998@users.noreply.github.com>
2022-11-27 16:10:45 +0100

    feat: delete endpoints

--- Backend/ReadMy/Application/Constants/LogMessages.cs
+++ Backend/ReadMy/Application/Constants/LogMessages.cs
@@ -3,4 +3,5 @@
 public static class LogMessages
 {
     public const string LogNotFound = "A bejegyzés nem található.";
+    public const string LogDeleteOnlyCreator = "A bejegyzést csak a létrehozó törölheti!";
 }
---
+++ Backend/ReadMy/Application/Eventing/Command/Commands/DeleteProjectCommand.cs
@@ -0,0 +1,16 @@
+﻿using Application.Eventing.Command.Dtos;
+using Application.Eventing.Command.Response;
+using MediatR;
+
+namespace Application.Eventing.Command.Commands
+{
+    public class DeleteProjectCommand : IRequest<DeleteEntityResponse>
+    {
+        public DeleteProjectCommand(string id)
+        {
+            Id = id;
+        }
+
+        public string Id { get; }
+    }
+}
