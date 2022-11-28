f826acb2f405e436643e2d3ea62dd44216b1fc2e
Csabi1998 <61312438+Csabi1998@users.noreply.github.com>
2022-11-28 10:25:33 +0100

    feat: Get own logs endpoint

---
+++ Backend/ReadMy/Application/Eventing/Query/Querys/GetLogsByTaskIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using Application.Eventing.Query.ViewModels;
+
+using MediatR;
+
+namespace Application.Eventing.Query.Querys
+{
+    public class GetLogsByTaskIdQuery : IRequest<LogsListViewModel>
+    {
+        public GetLogsByTaskIdQuery(string taskId)
+        {
+            TaskId = taskId;
+        }
+
+        public string TaskId { get; }
+    }
+}
