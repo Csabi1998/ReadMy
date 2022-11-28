3fc48e7434d80b8ca14d862f2db0820aee0eddba
Czeglédi Levente <czegledi.levente@wadjet.hu>
2022-11-28 08:47:22 +0100

    all project-related form functionality ready

--- Frontend/read-my/src/app/api/projects/project.service.ts
+++ Frontend/read-my/src/app/api/projects/project.service.ts
@@ -2,13 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, map, Observable, tap } from 'rxjs';
 import { ProjectResponse } from 'src/app/api/projects/models/projectResponse';
+import { AddProjectParticipantDto } from './models/addProjectParticipantDto';
 import { CreateProjectDto } from './models/createProjectDto';
 import { CreateProjectResponse } from './models/createProjectResponse';
+import { DeleteEntityResponse } from './models/deleteEntityResponse';
 import { ProjectsListResponse } from './models/projectsListResponse';
 import { UpdateProjectDto } from './models/updateProjectDto';
 import { UpdateProjectResponse } from './models/updateProjectResponse';
 
 import handleError from '../handleError';
+import { UpdateEntityResponse } from './models/updateEntityResponse';
 
 @Injectable({
   providedIn: 'root',
@@ -41,5 +44,30 @@ export class ProjectService {
       tap((response) => console.log(response)),
       catchError(handleError)
     );
+  }
+
+  deleteProject(id: string): Observable<DeleteEntityResponse> {
+    return this.httpClient.delete(`Projects/${id}`).pipe(
+      tap((response: any) => console.log(response)),
+      catchError(handleError)
+    );
+  }
+
+  getProjectById(projectId: string): Observable<ProjectResponse> {
+    return this.httpClient.get(`Projects/${projectId}`).pipe(
+      tap((response: any) => console.log(response)),
+      catchError(handleError)
+    );
+  }
+
+  addParticipant(
+    addProjectParticipantDto: AddProjectParticipantDto
+  ): Observable<UpdateEntityResponse> {
+    return this.httpClient
+      .put('Projects/participant', addProjectParticipantDto)
+      .pipe(
+        tap((response: any) => console.log(response)),
+        catchError(handleError)
+      );
   }
 }
